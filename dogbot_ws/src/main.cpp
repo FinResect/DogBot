57c60d2a8fa0d7fc90f1e9fefd215c868f4b558a
Fin_Resect <chenchengyue201@126.com>
2026-06-02 11:07:32 +0000

    release: Introducing official source code

--- dogbot_ws/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "../includes/hello.hpp"
-
-int main() {
-  Hello Hello;
-  Hello.HelloWorld();
-  return 0;
-}
--- dogbot_ws/tools/hello.cpp
+++
@@ -1,5 +0,0 @@
-#include "../includes/hello.hpp"
-#include <iostream>
-
-void Hello::HelloWorld() { std::cout << "Hello, World!" << std::endl; }
-void mmm() {};
